715bf28a315cdef01504df6487f98c2d2b4854a4
Rory McLean <rory.mclean@aderas.com>
2018-04-11 13:45:14 -0400

    Test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyAAhIYK5-ryzzFZuATcdKK9645KiO4iICc",
-  authDomain: "udemy2-9d370.firebaseapp.com",
-  databaseURL: "https://udemy2-9d370.firebaseio.com",
-  projectId: "udemy2-9d370",
-  storageBucket: "udemy2-9d370.appspot.com",
-  messagingSenderId: "445463999326"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
