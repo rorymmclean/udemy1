eec6e7bad066d9653def8ab4fb426c90596ce399
Rory McLean <rory.mclean@aderas.com>
2018-07-12 11:46:38 -0400

    stuff

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -7,7 +7,7 @@ export const ExpenseList = (props) => (
   <div className="content-container">
     <div className="list-header">
       <div className="show-for-mobile">Expenses</div>
-      <div className="show-for-desktop">My Expenses</div>
+      <div className="show-for-desktop">Outlays</div>
       <div className="show-for-desktop">Amount</div>
     </div>
     <div className="list-body">
