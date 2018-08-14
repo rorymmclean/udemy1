1729438ebfc974ba6ed6eb480600ba79e964cc4d
Rory McLean <rory.mclean@aderas.com>
2018-08-14 15:05:07 -0400

    LogRocket

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
 import LogRocket from 'logrocket';
-import LogRocket from 'logrocket';
 import setupLogRocketReact from 'logrocket-react';
 
 LogRocket.init('eqkymk/expenses');
